0d276911ccf95f8bb7366b79e17f12eed9ef186b
Vinicius Gagliano <vinicius.cg.gagliano@gmail.com>
2019-10-29 23:18:04 -0300

    Página para importação

---
+++ js/importacao-seguradora.js
@@ -0,0 +1,29 @@
+const appVue = new Vue({
+    el: '#appVue',
+    data: {
+        seguradora: 0,
+        tipoArquivo: 1,
+        arrayImportacao: []
+    },
+    mounted() {
+        $('#modalImportacao').on('hidden.bs.modal', () => { this.LimparCampos(); })
+    },
+    methods: {
+        OpenModal() { $('#modalImportacao').modal('show'); },
+        LimparCampos() {
+            this.seguradora = 0;
+            this.tipoArquivo = 1;
+        },
+        ValidarCampos() {
+            if (!parseInt(this.seguradora)) {
+                swal.fire('Atenção', `O campo seguradora deve ser preenchido`, 'warning');
+                return false;
+            }
+            console.log($('#arqSeguradora').val());
+
+            this.ImportarArquivo();
+        },
+        CarregarImportacao() { },
+        ImportarArquivo() { }
+    },
+});
