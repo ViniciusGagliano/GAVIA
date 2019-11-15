8c946e62c619ba90f892d54f0be0d47039fd9920
Vinicius Gagliano <vinicius.cg.gagliano@gmail.com>
2019-11-15 15:07:54 -0300

    Importacao arquivo

--- js/importacaoSinistro.js
+++ js/importacaoSinistro.js
@@ -3,7 +3,8 @@ const importacaoSinistroVue = new Vue({
     data: {
         seguradora: 0,
         tipoArquivo: 1,
-        arrayImportacao: []
+        arrayImportacao: [],
+        arquivo: null,
     },
     mounted() {
         $('#modalImportacao').on('hidden.bs.modal', () => { this.LimparCampos(); })
@@ -14,16 +15,63 @@ const importacaoSinistroVue = new Vue({
             this.seguradora = 0;
             this.tipoArquivo = 1;
         },
-        ValidarCampos() {
+        async ValidarCampos() {
             if (!parseInt(this.seguradora)) {
                 swal.fire('Atenção', `O campo seguradora deve ser preenchido`, 'warning');
                 return false;
             }
-            console.log($('#arqSeguradora').val());
 
-            this.ImportarArquivo();
+            const file = document.getElementById('arqSeguradora').files;
+            if (!file.length) {
+                swal.fire(`Atenção`, `Selecione algum arquivo`, `warning`);
+                return false;
+            }
+
+            const isValidExtension = /(\.xls)?(.xlsx)?$/;
+            if (!isValidExtension.exec(file[0].name)) {
+                swal.fire(`Atenção`, `Apenas arquivos de extensões .xls e .xlsx`, 'warning');
+                return false;
+            }
+
+            if (!typeof file[0].name === 'string') {
+                swal.fire(`Atenção`, `Nome do arquivo inválido`, 'warning');
+                return false;
+            }
+
+            $.LoadingOverlay('show');
+            this.arquivo = file[0];
+            try {
+                await this.ImportarArquivo();
+            } catch (error) {
+                console.log(error.responseText);
+            }
         },
-        CarregarImportacao() { },
-        ImportarArquivo() { }
+        async ImportarArquivo() {
+            return new Promise((resolve, reject) => {
+                let formData = new FormData();
+                formData.append(this.arquivo.name, this.arquivo);
+
+                $.ajax({
+                    type: 'POST',
+                    url: ``,
+                    data: formData,
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                    done: response => {
+                        this.arquivo = null;
+                        $('#arqSeguradora').val('');
+                        swal.fire('Sucesso', `Importação concluída`, 'success');
+                        resolve(response);
+                    },
+                    fail: error => {
+                        console.log(error);
+                        swal.fire('Erro', `Importação falhou`, 'error');
+                        reject(error);
+                    },
+                    always: _ => { $.LoadingOverlay('hide'); }
+                });
+            });
+        }
     },
 });
