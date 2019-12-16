6e4fc5d949d5ab4fd7b8ec419eeb04d97f91c7db
Vinicius Gagliano <vincus.gagliano@gmail.com>
2019-12-16 18:45:47 -0300

    x

--- api/api-basico/Entity/Acompanhamento/SinistroEntity.cs
+++ api/api-basico/Entity/Acompanhamento/SinistroEntity.cs
@@ -11,7 +11,23 @@ namespace Entity.Acompanhamento
     {
         public int Id { get; set; }
         public bool Ativo { get; set; }
-        public RegistroEntity Registro { get; set; }
+        public string NumeroSinistro { get; set; }
+        public string Processo { get; set; }
+        public string Bilhete { get; set; }
+        public string Referencia { get; set; }
+        public DateTime DataAtendimento { get; set; }
+        public DateTime DataOcorrencia { get; set; }
+        public string Voucher { get; set; }
+        public string Cobertura { get; set; }
+        public decimal CustoOriginal { get; set; }
+        public string TipoMoeda { get; set; }
+        public decimal ValorFinal { get; set; }
+        public decimal Fee { get; set; }
+        public string TipoMoedaFee { get; set; }
+        public decimal ValorDolar { get; set; }
+        public decimal ValorReal { get; set; }
+        public double Dolar { get; set; }
+        public int RegistroId { get; set; }
         public SeguradoEntity Segurado { get; set; }
         public NotaDebitoEntity NotaDebito { get; set; }
         public PatrocinioEntity Patrocinio { get; set; }
