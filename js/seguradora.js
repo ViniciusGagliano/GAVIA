14b0221d8b5c2b3c82da0e1dce69a2937e6a722c
Vinicius Gagliano <vincus.gagliano@gmail.com>
2019-12-16 21:45:41 -0300

    Seguradora Tela

--- js/seguradora.js
+++ js/seguradora.js
@@ -1,8 +1,8 @@
 const urlGlobal = `https://localhost:44318/basico/seguradoras`;
-const axiosSettings = {
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded', "Access-Control-Allow-Origin": true, },
-    withCredentials: false,
-};
+
+$(document).ready(() => {
+    $('#cnpj').mask('00.000.000/0000-00', { reverse: true });
+})
 
 const seguradoraVue = new Vue({
     el: '#seguradoraVue',
@@ -21,6 +21,9 @@ const seguradoraVue = new Vue({
     },
     mounted() { $.LoadingOverlay('hide'); },
     methods: {
+        ApenasNumeros(event) {
+            if (!/\d/.test(event.key)) { return event.preventDefault(); }
+        },
         ValidarDados() {
             if (!this.nome || !this.cnpj) {
                 Swal.fire('Atenção', `Os campos nome e cnpj são obrigatórios`, 'warning');
@@ -29,48 +32,48 @@ const seguradoraVue = new Vue({
 
             (this.id) ? this.Editar() : this.Cadastrar();
         },
+        LimparCampos() {
+            this.id = 0;
+            this.nome = '';
+            this.cnpj = '';
+            this.antecipacao = false;
+        },
         PreencherCampos(id) {
             this.id = id;
-            let seguradora = this.arraySeguradora.filter(s => s.Id === id);
-            this.nome = seguradora.nome;
-            this.cnpj = seguradora.cnpj;
-            this.antecipacao = seguradora.antecipacao;
+            let seguradora = this.arraySeguradora.filter(s => s.Id === id)[0];
+            this.nome = seguradora.Nome;
+            this.cnpj = seguradora.CNPJ;
+            this.antecipacao = seguradora.Antecipacao;
         },
         async Cadastrar() {
             $.LoadingOverlay('show');
-            // axios.post(`${urlGlobal}`, `Nome=${this.nome}&CNPJ=${this.cnpj}&Antecipacao=${this.antecipacao}`, axiosSettings).then(_ => {
-            //     Swal.fire('Sucesso', 'Cadastro realizado com sucesso', 'success');
-            //     this.Carregar();
-            // }).catch(error => {
-            //     Swal.fire('Erro', 'Algo inesperado ocorreu', 'error');
-            //     console.log(error);
-            // }).finally(_ => $.LoadingOverlay('hide'));
-            await $.ajax({
-                type: 'POST',
-                crossDomain: true,
-                url: `${urlGlobal}`,
-                data: {
-                    Nome: this.nome,
-                    CNPJ: this.cnpj,
-                    Antecipacao: this.antecipacao
-                }
-            }).done(_ => {
+            let CNPJ = this.cnpj.split('.').join('').split('/').join('').split('-').join('');
+
+            axios.post(`${urlGlobal}`, {
+                Nome: this.nome,
+                CNPJ,
+                Antecipacao: this.antecipacao
+            }).then(_ => {
                 Swal.fire('Sucesso', 'Cadastro realizado com sucesso', 'success');
                 this.Carregar();
-            }).error(error => {
+                this.LimparCampos();
+            }).catch(error => {
                 Swal.fire('Erro', 'Algo inesperado ocorreu', 'error');
                 console.log(error);
-            }).always(_ => $.LoadingOverlay('hide'));
+            }).finally(_ => $.LoadingOverlay('hide'));
         },
         async Editar() {
             $.LoadingOverlay('show');
+            let CNPJ = this.cnpj.split('.').join('').split('/').join('').split('-').join('');
+
             await axios.put(`${urlGlobal}/${this.id}`, {
                 Nome: this.nome,
-                CNPJ: this.cnpj,
+                CNPJ,
                 Antecipacao: this.antecipacao
-            }, axiosSettings).then(_ => {
+            }).then(_ => {
                 Swal.fire('Sucesso', 'Dados alterado com sucesso', 'success');
                 this.Carregar();
+                this.LimparCampos();
             }).catch(error => {
                 Swal.fire('Erro', 'Algo inesperado ocorreu', 'error');
                 console.log(error);
@@ -82,12 +85,11 @@ const seguradoraVue = new Vue({
                 text: `Essa operação não poderá ser desfeita`,
                 icon: `warning`,
                 showCancelButton: true,
+                cancelButtonColor: '#d33',
             }).then(result => {
                 if (result.value) {
                     $.LoadingOverlay('show');
-                    axios.delete(`${urlGlobal}/${id}`, {
-                        withCredentials: true
-                    }).then(_ => {
+                    axios.delete(`${urlGlobal}/${id}`).then(_ => {
                         Swal.fire('Sucesso', 'Seguradora excluída com sucesso', 'success');
                         this.Carregar();
                     }).catch(error => {
@@ -98,25 +100,17 @@ const seguradoraVue = new Vue({
             })
         },
         Carregar() {
-            axios.get(`${urlGlobal}`, axiosSettings).then(response => {
+            axios.get(`${urlGlobal}`).then(response => {
                 if (!response.data) {
                     Swal.fire('Nenhum resultado encontrado', '', 'warning');
                     return false;
                 }
+                console.log(response.data);
 
                 this.arraySeguradora = response.data['ativos'];
             }).catch(error => {
                 console.log(error);
             });
-            // $.ajax({
-            //     type: 'GET',
-            //     crossDomain: true,
-            //     url: `${urlGlobal}`
-            // }).done(response => {
-            //     console.log(response);
-            // }).error(error => {
-            //     console.log(error);
-            // });
         },
     },
 });
